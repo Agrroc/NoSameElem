55b5e8ccb22f64d96f5201e133d127634c6d2314
agrroc <a962758876@sina.com>
2018-09-08 11:03:10 +0800

    用链表实现输入字符串，并且没有相同元素

--- NoSameElem/main.cpp
+++ NoSameElem/main.cpp
@@ -7,9 +7,59 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+
+typedef struct LNode{
+    char data;
+    struct LNode *next;
+}LNode;
+
+void creatNoSameElem(LNode *&head)
+{
+    head=(LNode *)malloc(sizeof(LNode));
+    head->next=NULL;
+    LNode *p;
+    int n;
+    char ch;
+    printf("请输入字母的个数：");
+//    scanf("%d\n",&n);
+    std::cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        printf("请输入字母：");
+//        scanf("%c",&ch);
+        std::cin>>ch;
+        p=head->next;
+        while (p!=NULL) {
+            if (p->data==ch) {
+                break;
+            }
+            p=p->next;
+        }
+        if (p==NULL) {
+            p=(LNode *)malloc(sizeof(LNode));
+            p->data=ch;
+            p->next=head->next;
+            head->next=p;
+        }
+    }
+}
+
+void travelElem(LNode *head)
+{
+    LNode *tr=head->next;
+    while (tr!=NULL) {
+        printf("%c",tr->data);
+        tr=tr->next;
+    }
+    printf("\n");
+    return ;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    LNode *head=NULL;
+    creatNoSameElem(head);
+    travelElem(head);
     return 0;
 }
